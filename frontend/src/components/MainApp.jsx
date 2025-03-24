0fed42e0a8048e7b8eac3cb41ba0fa6925b7e573
xtrajank <owen.rasor8@gmal.com>
2025-03-23 22:53:20 -0600

    fix main url

--- frontend/src/components/MainApp.jsx
+++ frontend/src/components/MainApp.jsx
@@ -4,7 +4,7 @@ import FileUploader from './FileUploader';
 import { Button, Box, Typography, FormControlLabel, FormGroup, Checkbox, Switch, RadioGroup, Radio } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const MAIN_URL = ""
+const MAIN_URL = "https://extractify-omwu.onrender.com"
 
 function csvSafeJoin(arr) {
   return arr.map(cell => (cell == null ? '' : String(cell))).join('\t');
